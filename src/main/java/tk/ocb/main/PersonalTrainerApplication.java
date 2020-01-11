e29efeea446a70677a3bc1eba9675ff3705df1c3
Apple <apple@Apples-MacBook-Pro.local>
2020-01-11 12:15:31 +0300

    project initialization

---
+++ src/main/java/tk/ocb/main/PersonalTrainerApplication.java
@@ -0,0 +1,13 @@
+package tk.ocb.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTrainerApplication.class, args);
+	}
+
+}
